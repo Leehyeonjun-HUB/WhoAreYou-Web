3ae72d2b199da0e5924fb7f64314a270b1b9d838
Leehyeonjun-HUB <ksc4682@naver.com>
2019-09-16 12:46:33 +0900

    webproject

---
+++ WebProject/src/model/dto/ModiDTO.java
@@ -0,0 +1,26 @@
+package model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class ModiDTO {
+
+
+
+
+		private String id;
+		private String pw;
+		private String name;
+		private String age;
+		private String birthday;
+		private String address;
+		private String job;
+		private String height;
+
+	}
